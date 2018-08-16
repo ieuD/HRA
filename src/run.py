b9beb4ec79bd1a793d4be2bb6bd67288bcdb23c5
jyri <ibrahim.cankurtran@ogrenci.selcuk.edu.tr>
2018-08-16 08:29:55 -0400

    works

---
+++ src/run.py
@@ -0,0 +1,123 @@
+import importlib
+import sys
+import csv
+import datetime
+import os
+
+
+
+from nupic.data.inference_shifter import InferenceShifter
+from nupic.frameworks.opf.model_factory import ModelFactory
+
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+MODEL_PARAMS = {'aggregationInfo': {'days': 0,
+                     'fields': [(u'timestamp', 'first'),
+                                (u'heartrate', 'sum')],
+                     'hours': 0,
+                     'microseconds': 0,
+                     'milliseconds': 0,
+                     'minutes': 5,
+                     'months': 0,
+                     'seconds': 0,
+                     'weeks': 0,
+                     'years': 0},
+ 'model': 'HTMPrediction',
+ 'modelParams': {'anomalyParams': {u'anomalyCacheRecords': None,
+                                   u'autoDetectThreshold': None,
+                                   u'autoDetectWaitRecords': None},
+                 'clParams': {'alpha': 0.050050000000000004,
+                              'verbosity': 0,
+                              'regionName': 'SDRClassifierRegion',
+                              'steps': '1'},
+                 'inferenceType': 'TemporalAnomaly',
+                 'sensorParams': {'encoders': {u'raw_value': None,
+                                               u'timestamp_dayOfWeek': None,
+                                               u'timestamp_timeOfDay': None,
+                                               u'timestamp_weekend': None,
+                                               u'heartrate': {'clipInput': True,
+                                                                  'fieldname': 'heartrate',
+                                                                  'n': 272,
+                                                                  'name': 'heartrate',
+                                                                  'type': 'AdaptiveScalarEncoder',
+                                                                  'w': 21
+                                                                  }},
+                                  'sensorAutoReset': None,
+                                  'verbosity': 0
+                                  },
+                 'spEnable': True,
+                 'spParams': {'columnCount': 2048,
+                              'globalInhibition': 1,
+                              'inputWidth': 0,
+                              'numActiveColumnsPerInhArea': 40,
+                              'potentialPct': 0.8,
+                              'seed': 1956,
+                              'spVerbosity': 0,
+                              'spatialImp': 'cpp',
+                              'synPermActiveInc': 0.05,
+                              'synPermConnected': 0.1,
+                              'synPermInactiveDec': 0.05015},
+                 'tmEnable': True,
+                 'tmParams': {'activationThreshold': 14,
+                              'cellsPerColumn': 32,
+                              'columnCount': 2048,
+                              'globalDecay': 0.0,
+                              'initialPerm': 0.21,
+                              'inputWidth': 2048,
+                              'maxAge': 0,
+                              'maxSegmentsPerCell': 128,
+                              'maxSynapsesPerSegment': 32,
+                              'minThreshold': 11,
+                              'newSynapseCount': 20,
+                              'outputType': 'normal',
+                              'pamLength': 3,
+                              'permanenceDec': 0.1,
+                              'permanenceInc': 0.1,
+                              'seed': 1960,
+                              'temporalImp': 'cpp',
+                              'verbosity': 0},
+                 'trainSPNetOnlyIfRequested': False},
+ 'predictAheadTime': None,
+'version': 1}
+
+
+
+def createModel(modelParams= MODEL_PARAMS):
+    model = ModelFactory.create(modelParams)
+    model.enableInference({"predictedField" : "heartrate"})
+    return model
+
+
+def runIoThroughNupic(inputData , model ):
+    inputFile = open("../data/heart-rate.csv" , "rb")
+    csvReader = csv.reader(inputFile)
+    csvReader.next()
+
+    counter = 0
+
+    with open("./output.csv" , "w") as file:
+        for row in csvReader:
+            counter +=1
+
+            if(counter %100 ==0):
+                print "%i lines..." %counter
+
+
+                rate = float(row[1])
+                timestamp = str(row[0])
+
+                result = model.run({
+                "timestamp" : timestamp,
+                "heartrate" : rate
+                })
+
+
+                prediction = result.inferences["multiStepBestPredictions"][1]
+                anomalyScore = result.inferences["anomalyScore"]
+
+                file.write(timestamp + "-->"+ str(prediction) + "-->" + str(anomalyScore) + "\n")
+
+
+
+if __name__ == "__main__":
+    runIoThroughNupic(inputData="../data/heart-rate.csv", model = createModel(modelParams= MODEL_PARAMS))
