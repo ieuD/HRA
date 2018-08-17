0e0cd7dcadfd8271db3cf2e21472f103980c4ced
jyri <ibrahim.cankurtran@ogrenci.selcuk.edu.tr>
2018-08-17 08:37:37 -0400

    check test

--- src/test.py
+++ src/test.py
@@ -1,55 +1,63 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import csv
+#!/usr/bin/env python
+# Plot a graph of Data which is comming in on the fly
+# uses pylab
+# Author: Norbert Feurle
+# Date: 12.1.2012
+# License: if you get any profit from this then please share it with me
+import pylab
+from pylab import *
 
-def data_gen():
+xAchse=pylab.arange(0,100,1)
+yAchse=pylab.array([0]*100)
 
-    t = 0
-    x = []
-    y = []
-    with open("../data/heart-rate.csv" , "r") as file:
-        reader = csv.reader(file , delimiter=",")
-        reader.next()
+fig = pylab.figure(1)
+ax = fig.add_subplot(111)
+ax.grid(True)
+ax.set_title("Realtime Waveform Plot")
+ax.set_xlabel("Time")
+ax.set_ylabel("Amplitude")
+ax.axis([0,100,-1.5,1.5])
+line1=ax.plot(xAchse,yAchse,'-')
 
-        for row in reader:
-            y.append(float(row[1]))
-            x.append(t)
-            t+=1
-            yield x,y
+manager = pylab.get_current_fig_manager()
 
+values=[]
+values = [0 for x in range(100)]
 
+Ta=0.01
+fa=1.0/Ta
+fcos=3.5
 
+Konstant=cos(2*pi*fcos*Ta)
+T0=1.0
+T1=Konstant
 
+def SinwaveformGenerator(arg):
+  global values,T1,Konstant,T0
+  #ohmegaCos=arccos(T1)/Ta
+  #print "fcos=", ohmegaCos/(2*pi), "Hz"
 
-def init():
-    ax.set_ylim(-1.1, 1.1)
-    ax.set_xlim(0, 10)
-    del xdata[:]
-    del ydata[:]
-    line.set_data(xdata, ydata)
-    return line,
+  Tnext=((Konstant*T1)*2)-T0
+  if len(values)%100>70:
+    values.append(random()*2-1)
+  else:
+    values.append(Tnext)
+  T0=T1
+  T1=Tnext
 
-fig, ax = plt.subplots()
-line, = ax.plot([], [], lw=2)
-ax.grid()
-xdata, ydata = [], []
+def RealtimePloter(arg):
+  global values
+  CurrentXAxis=pylab.arange(len(values)-100,len(values),1)
+  line1[0].set_data(CurrentXAxis,pylab.array(values[-100:]))
+  ax.axis([CurrentXAxis.min(),CurrentXAxis.max(),-1.5,1.5])
+  manager.canvas.draw()
+  #manager.show()
 
+timer = fig.canvas.new_timer(interval=20)
+timer.add_callback(RealtimePloter, ())
+timer2 = fig.canvas.new_timer(interval=20)
+timer2.add_callback(SinwaveformGenerator, ())
+timer.start()
+timer2.start()
 
-def run(data):
-    # update the data
-    t, y = data
-    xdata.append(t)
-    ydata.append(y)
-    xmin, xmax = ax.get_xlim()
-
-    if t >= xmax:
-        ax.set_xlim(xmin, 2*xmax)
-        ax.figure.canvas.draw()
-    line.set_data(xdata, ydata)
-
-    return line,
-
-ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
-                              repeat=False, init_func=init)
-plt.show()
+pylab.show()
